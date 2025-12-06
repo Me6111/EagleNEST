cfdd478805449546faab02b6c930489b3203d1f6
Me6111 <maksymmpawlowski@gmail.com>
2025-12-06 17:13:03 +0000

    deployment

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,14 @@
-
-
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
 import * as dotenv from 'dotenv';
-dotenv.config();
 
+dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT || 3000, '0.0.0.0');
+  const port = process.env.PORT || 3000;
+  console.log(`Server is listening on port ${port}`);
+  await app.listen(port, '0.0.0.0');
 }
+
 bootstrap();
